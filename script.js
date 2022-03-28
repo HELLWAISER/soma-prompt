de4dfaf6c7283b2b030cbb146e680fbc36d695b9
Vitor Rodrigues Gomes <vitor_jackson1992@hotmail.com>
2022-03-27 22:24:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//alert('Mensagem qualquer');
+//atenção isto não esta rodando no node
+
+let num1 = prompt('Digite o Primeiro número');
+let num2 = prompt('Digite o segundo número');
+num1 = Number(num1); 
+num2 = Number(num2);
+console.log(num1 + num2);
+const resultado = num1 + num2;
+alert(`O resultado da sua conta foi = ${resultado}`);
